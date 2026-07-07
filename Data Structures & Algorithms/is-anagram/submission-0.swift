08327f2275c77840b10bf401ac954d59b23a2934
Sanzhar Karbayev <50317537+SanzharKarbayev@users.noreply.github.com>
2026-07-07 06:24:06 +0500

    Update submission-0.swift

--- Data Structures & Algorithms/is-anagram/submission-0.swift
+++ Data Structures & Algorithms/is-anagram/submission-0.swift
@@ -3,9 +3,3 @@ class Solution {
         return s.sorted() == t.sorted()
     }
 }
-
-// class Solution {
-//     func isAnagram(_ s: String, _ t: String) -> Bool {
-//         return Set(s) == Set(t)
-//     }
-// }
